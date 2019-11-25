b3deb81b9d477b5abba65858eb575d408d17d2e4
Hashir Sarwar <hashirsarwarch@gmail.com>
2019-11-25 23:05:21 +0500

    Changed package name and updated dependencies.

--- app/src/main/java/com/example/groupme/MessageAndTime.java
+++
@@ -1,8 +0,0 @@
-package com.example.groupme;
-
-class MessageAndTime {
-
-    String message;
-    String messageTime;
-    String messageType;
-}
--- app/src/main/java/com/example/groupme/WrapperForHomeList.java
+++
@@ -1,8 +0,0 @@
-package com.example.groupme;
-
-import java.util.List;
-
-class WrapperForHomeList {
-    List<Group> a;
-    List<MessageAndTime> b;
-}
